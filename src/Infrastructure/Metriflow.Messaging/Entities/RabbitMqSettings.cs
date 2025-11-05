ff801c6cd15c7702c442a1ac6c28d4566e1267a1
MustaphaAlaa <mostafaalaa11998@gmail.com>
2025-11-05 19:57:22 +0200

    Introduces RabbitMQ messaging component
    Provides a dedicated infrastructure for connecting to RabbitMQ and publishing messages.
    
    *   Defines an `IRabbitMQConnection` interface to abstract RabbitMQ operations, enhancing testability and maintainability.
    *   Implements `RabbitMQConnection` using the `RabbitMQ.Client` library, handling connection establishment, channel management, and message publishing.
    *   Utilizes `RabbitMqSettings` for externalizing connection parameters, allowing for flexible configuration.
    *   Integrates `Microsoft.Extensions.Logging` for improved observability of messaging operations.
    *   Enables declaring exchanges and publishing messages to specific routing keys.

---
+++ src/Infrastructure/Metriflow.Messaging/Entities/RabbitMqSettings.cs
@@ -0,0 +1,9 @@
+namespace Metriflow.Messaging.Entities;
+
+public class RabbitMqSettings
+{
+    public string HostName { get; set; } = string.Empty;
+    public int Port { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
