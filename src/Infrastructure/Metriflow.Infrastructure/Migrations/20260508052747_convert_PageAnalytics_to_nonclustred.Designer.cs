dbbf7afab45517f8c0478c78230b7afae461e9ee
MustaphaAlaa <MostafaAlaa11998@gmail.com>
2026-05-10 08:10:34 +0300

    before remigration

--- src/Infrastructure/Metriflow.Infrastructure/Migrations/20260508052747_convert_PageAnalytics_to_nonclustred.Designer.cs
+++ src/Infrastructure/Metriflow.Infrastructure/Migrations/20260508052747_convert_PageAnalytics_to_nonclustred.Designer.cs
@@ -4,6 +4,7 @@ using Metriflow.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Metriflow.Infrastructure.Migrations
 {
     [DbContext(typeof(MetriflowDbContext))]
-    partial class MetriflowDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260508052747_convert_PageAnalytics_to_nonclustred")]
+    partial class convert_PageAnalytics_to_nonclustred
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Infrastructure/Metriflow.Infrastructure/Migrations/20260508052747_convert_PageAnalytics_to_nonclustred.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Metriflow.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class convert_PageAnalytics_to_nonclustred : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PageAnalytics",
+                table: "PageAnalytics");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PageAnalytics",
+                table: "PageAnalytics",
+                columns: new[] { "PageId", "Date", "Intervals" })
+                .Annotation("SqlServer:Clustered", false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PageAnalytics",
+                table: "PageAnalytics");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PageAnalytics",
+                table: "PageAnalytics",
+                columns: new[] { "PageId", "Date", "Intervals" });
+        }
+    }
+}
